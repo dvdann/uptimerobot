f5008ec39781b12f1bf34490da8c80df36e20a47
Daniel Septyadi <danielseptyadi@gmail.com>
2020-12-11 08:52:00 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const axios = require("axios")
-const urls = ["https://suave-everlasting-clipper.glitch.me"]
+const urls = ["https://dev-dann.glitch.me"]
 setInterval(function() {
             urls.forEach(url => {
             axios.get(url).then(console.log("Pong at " + Date.now())).catch(() => {});
