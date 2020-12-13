d92ac2ca36f7c4218e316f66d59624ae7e203c66
Daniel Septyadi <danielseptyadi@gmail.com>
2020-12-13 21:40:08 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const axios = require("axios")
-const urls = ["https://dev-dann.glitch.me"]
+const urls = ["https://ear-candy.glitch.me"]
 setInterval(function() {
             urls.forEach(url => {
             axios.get(url).then(console.log("Pong at " + Date.now())).catch(() => {});
